c0c0fc977f454ca2c855b6abdfc97486637b8250
Tharindu Lakshitha <tharindulakshitha@arimaclanka.com>
2023-07-23 20:33:47 +0530

    cleanup

--- config.js
+++ config.js
@@ -4,6 +4,7 @@ import mongoose from "mongoose";
 dotenv.config();
 
 const config = {
+    app_name: process.env['APP_NAME'],
     port: process.env['PORT'] ?? 3000,
     db_uri: process.env['DB_URI'] ?? 'mongodb://localhost:27017/docker',
     db_options: {
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import app from "./app.js";
+import config from "../config.js";
 
 app.listen(3000, () =>{
-    console.log("App Started on Port 3000")
+    console.log(config.app_name+" Started on Port 3000")
 })
